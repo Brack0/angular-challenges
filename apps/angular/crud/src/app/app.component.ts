cb988a2f56151508fdfb40921b272d4a5279fe95
brack0 <9846217+Brack0@users.noreply.github.com>
2023-11-13 13:31:55 +0100

    feat(todo): add todo type

--- apps/angular/crud/src/app/app.component.ts
+++ apps/angular/crud/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { randText } from '@ngneat/falso';
+import { Todo } from './todo/todo.model';
 
 @Component({
   standalone: true,
@@ -16,26 +17,26 @@ import { randText } from '@ngneat/falso';
   styles: [],
 })
 export class AppComponent implements OnInit {
-  todos!: any[];
+  todos!: Todo[];
 
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
     this.http
-      .get<any[]>('https://jsonplaceholder.typicode.com/todos')
+      .get<Todo[]>('https://jsonplaceholder.typicode.com/todos')
       .subscribe((todos) => {
         this.todos = todos;
       });
   }
 
-  update(todo: any) {
+  update(todo: Todo) {
     this.http
-      .put<any>(
+      .put<Todo>(
         `https://jsonplaceholder.typicode.com/todos/${todo.id}`,
         JSON.stringify({
-          todo: todo.id,
+          id: todo.id,
           title: randText(),
-          body: todo.body,
+          completed: todo.completed,
           userId: todo.userId,
         }),
         {
@@ -44,7 +45,7 @@ export class AppComponent implements OnInit {
           },
         }
       )
-      .subscribe((todoUpdated: any) => {
+      .subscribe((todoUpdated: Todo) => {
         this.todos[todoUpdated.id - 1] = todoUpdated;
       });
   }
